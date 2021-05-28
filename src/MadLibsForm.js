d7ab90b29ae37245ccd0cbbea2f8eda7ff4acd54
scottkoons <scottkoons@gmail.com>
2021-05-28 13:34:31 -0600

    added css

--- src/MadLibsForm.js
+++ src/MadLibsForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './MadLibsForm.css';
 
 const MadLibsForm = ({ setShowStory, createStory }) => {
   const INITIAL_STATE = { noun: '', noun2: '', adjective: '', color: '' };
--- src/MadLibsStory.js
+++ src/MadLibsStory.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './MadLibsStory.css';
 
 const MadLibsStory = ({ setShowStory, setStory, story }) => {
   const restart = () => {
